4a6d3b3cea2b46e9aa1dbfda6974f613237e4953
feoolga <kirianova_ov@mail.ru>
2023-05-30 20:29:57 +0300

    modal_weather

---
+++ js/modal_weather.js
@@ -0,0 +1,46 @@
+const weatherBlock = document.querySelector('#weather')
+
+async function loadWeather() {
+	weatherBlock.innerHTML = '<div class="loading"> <img src="img/load.gif" alt="Loading..."></div>';
+
+	const server = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Gorokhovets&appid=5c2ab0f2e6c2ad23c076711877a90062'
+
+	const response = await fetch(server, { method: 'GET' })
+
+	const responserezult = await response.json()
+
+	if (response.ok) {
+		getWeather(responserezult)
+	} else {
+		weatherBlock.innerHTML = responserezult.message;
+	}
+}
+
+function getWeather(data) {
+	console.log(data);
+	let location;
+	if (data.name === 'Gorokhovets') {
+		location = 'Гороховец';
+	}
+	else {
+		location = data.name;
+	}
+
+	let temp = Math.round(data.main.temp)
+	let weath = data.weather[0].main
+	let weatherIcon = data.weather[0].icon
+
+	const template = `
+		<div class="city">${location}</div>
+		<div class="row">
+			<div>${weath}</div>
+			<img class="icon" src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="">
+		</div>
+		<div class="temper">${temp}</div>
+	`
+	weatherBlock.innerHTML = template;
+}
+
+if (weatherBlock) {
+	loadWeather()
+}
---
+++ js/modals.js
@@ -0,0 +1,7 @@
+let modalLinks = document.querySelectorAll('.modal-link')
+let body = document.querySelector('body')
+
+
+let unlock = true
+
+let timeout = 800
