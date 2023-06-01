a85276909ef775934d48671a5947152b4dcbdea0
feoolga <kirianova_ov@mail.ru>
2023-06-01 16:31:34 +0300

    modals begin

--- js/modals.js
+++ js/modals.js
@@ -5,3 +5,12 @@ let body = document.querySelector('body')
 let unlock = true
 
 let timeout = 800
+
+if(modalLinks.length > 0){
+    for (let index = 0; index < modalLinks.length; index++){
+        let modalLinK = modalLinks[index]
+        modalLinK.addEventListener("click", function(e){
+            let modalName = modalLinK.getAttribute('href').replace('#', '')
+        })
+    }
+}
